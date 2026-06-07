8a5dc2105331256fb00194fbd6a2df40b8507e46
Aaryan Ramteke <ramtekearyan1805@gmail.com>
2026-06-07 12:54:45 +0530

    fix: dict key status

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ response=$(curl -s http://localhost:8000/health)
 
 echo $response
 
-if response['status'] == 'healthy'; then
+if response['Status'] == 'healthy'; then
 	echo "health check passed"
 	exit 0
 else
-	echo "Health chcek failed"
+	echo "Health check failed"
 	exit 1
 fi
